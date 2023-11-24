64e7b73970a58423541aaa6bdb4fdd8acb5313eb
shanlinmin <shanlinmin@qq.com>
2023-11-24 13:29:53 +0800

    [add] 对SFINAE奇偶判断提交加注释

--- src/40.SFINAE/40-7.cpp
+++ src/40.SFINAE/40-7.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+// 试了一下，后面的等于 0 可以不要
+// template <int I> void foo(char(*)[I % 2 == 0]) {
 template <int I> void foo(char(*)[I % 2 == 0] = 0) {
 	std::cout << "I % 2 == 0" << std::endl;
 }
@@ -14,3 +16,15 @@ int main()
 	foo<2>(&a);
 	foo<3>(&a);
 }
+
+
+/* 
+❯ cd src/40.SFINAE
+❯ g++ 40-7.cpp -o 40-7.out -std=c++11
+❯ ./40-7.out
+I % 2 == 1
+I % 2 == 0
+I % 2 == 1
+
+这里是谁替换成功选哪个函数
+ */
